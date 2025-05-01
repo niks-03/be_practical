3835074ec4d50115267e5441cf80e74ffef8da27
Nikhil Ingale <121450067+niks-03@users.noreply.github.com>
2025-05-01 23:12:54 +0530

    Update server.py

--- CL-3/prac-1/server.py
+++ CL-3/prac-1/server.py
@@ -1,3 +1,5 @@
+#Design a distributed application using RPC for remote computation where client submits an integer
+# value to the server and server calculates factorial and returns the result to the client program.
 from xmlrpc.server import SimpleXMLRPCServer
 
 class FactorialServer:
